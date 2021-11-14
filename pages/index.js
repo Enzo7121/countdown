4adc994556ae3c5a69a8a7b74b88cdf73a970311
Enzo7121 <enzo.lanus@gmail.com>
2021-11-14 20:20:47 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,9 @@ const calculateTimeLeft = () => {
       hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
       minutes: Math.floor((difference / 1000 / 60) % 60),
       seconds: Math.floor((difference / 1000) % 60),
-    };
+    }
+  }else{
+    return {days: 0, hours: 0, minutes: 0, seconds: 0}
   }
 };
 
